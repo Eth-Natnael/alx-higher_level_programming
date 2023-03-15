e0919be398af37d4cf0b3805c9aab15d8c694b0a
Eth-Natnael <117724829+Eth-Natnael@users.noreply.github.com>
2023-03-15 12:29:11 +0300

    create files

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-
-
 def square_matrix_simple(matrix=[]):
-    """
-    wordA function that computes the square
-    value of all integers of a matrix.
-    """
-    new_matrix = []
-    for col in matrix:
-        result = list(map(lambda x: x**2, col))
-        new_matrix.append(result)
-    return new_matrix
+    response = []
+    for row in matrix:
+        sub_matrix = map(lambda num: num**2, row)
+        response.append(list(sub_matrix))
+    return response
